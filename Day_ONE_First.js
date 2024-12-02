7c71b5e3d7fea77f43b9b26e704630c50de2561d
Lewis <lewisbirch2@googlemail.com>
2024-12-02 12:15:12 +0000

    Completed Day ONE First

---
+++ Day_ONE_First.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+async function final() {
+  let leftString = [];
+  let rightString = [];
+  let difference = 0;
+  const rightRegex = /[0-9]+\n/g;
+  const leftRegex = /^[0-9]+/gm;
+
+  await fs.readFile("./assets/Day_ONE.txt", "utf8", (err, data) => {
+    for (let i = 0; i < data.match(rightRegex).length; i++) {
+      rightString.push(data.match(rightRegex)[i].slice(0, 5));
+    }
+    rightString.sort((x, y) => x - y);
+
+    for (let i = 0; i < data.match(leftRegex).length; i++) {
+      leftString.push(data.match(leftRegex)[i].slice(0, 5));
+    }
+    leftString.sort((x, y) => x - y);
+
+    for (let i = 0; i < rightString.length; i++) {
+      if (Number(rightString[i]) > Number(leftString[i])) {
+        difference += Number(rightString[i]) - Number(leftString[i]);
+      }
+      if (Number(rightString[i]) < Number(leftString[i])) {
+        difference += Number(leftString[i]) - Number(rightString[i]);
+      }
+      if (Number(rightString[i]) === Number(leftString[i])) {
+        difference += 0;
+      }
+    }
+    console.log(difference);
+  });
+
+  return;
+}
+
+final();
+
+//   1579939   :)
